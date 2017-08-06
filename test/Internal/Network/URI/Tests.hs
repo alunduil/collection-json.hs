3a9d7595ab0b519e5ede3373ac22ecbac032d27e
Alex Brandt <alunduil@alunduil.com>
2017-08-05 21:10:17 -0500

    import <$> from Control.Applicative
    Older ghc versions didn't include <$> in prelude.  To keep backwards
    compatibility for a short while, we'll include that import.

--- test/Internal/Network/URI/Tests.hs
+++ test/Internal/Network/URI/Tests.hs
@@ -12,6 +12,7 @@ A collection of 'Arbitrary' instances for 'URI'.
 -}
 module Internal.Network.URI.Tests where
 
+import Control.Applicative ((<$>))
 import Data.Maybe (fromJust)
 import Network.URI (parseURIReference, URI)
 import Test.QuickCheck (Arbitrary (arbitrary), elements)
