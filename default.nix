dcba700b495552a5d7e5220bfd6c2049fe2d18ef
Alex Brandt <alunduil@alunduil.com>
2017-11-03 19:41:24 -0500

    add nix derivations (shell and package)

---
+++ default.nix
@@ -0,0 +1,11 @@
+{ nixpkgs ? import <nixpkgs> { }
+, compiler ? "default"
+}:
+let
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+in
+  haskellPackages.callPackage ./collection-json.nix { }
