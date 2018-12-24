5716c06c4daedd849f16731834751922d380349c
Alex Brandt <alunduil@gmail.com>
2018-12-24 10:01:12 -0800

    Use autogenerated nix-shell environment.

--- nix/network-arbitrary.nix
+++ nix/network-arbitrary.nix
@@ -1,15 +1,11 @@
-{ mkDerivation, base, bytestring, case-insensitive, fetchgit, hspec
-, http-media, http-types, network-uri, QuickCheck, stdenv
-, test-invariant
+{ mkDerivation, base, bytestring, case-insensitive, hspec
+, hspec-discover, http-media, http-types, network-uri, QuickCheck
+, stdenv, test-invariant
 }:
 mkDerivation {
   pname = "network-arbitrary";
-  version = "0.3.0.0";
-  src = fetchgit {
-    url = "https://github.com/alunduil/network-arbitrary";
-    sha256 = "19fdkzhmjrh24c40c266nk8wzqwb12hmi05kwb764a9xjjxy4ymm";
-    rev = "e1b2fbfb0c7923b2c900d3cd14f9d5d5bb8029dc";
-  };
+  version = "0.4.0.1";
+  sha256 = "697e918907f3946edadd30246c041fe4c5d77f24d47eb5d165565091df303498";
   libraryHaskellDepends = [
     base bytestring http-media http-types network-uri QuickCheck
   ];
@@ -17,6 +13,7 @@ mkDerivation {
     base bytestring case-insensitive hspec http-media http-types
     network-uri QuickCheck test-invariant
   ];
+  testToolDepends = [ hspec-discover ];
   homepage = "https://github.com/alunduil/network-arbitrary";
   description = "Arbitrary Instances for Network Types";
   license = stdenv.lib.licenses.mit;
