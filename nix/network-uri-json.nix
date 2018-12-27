ea84665e98d366f6919daefc01f4267ebb100a2a
Alex Brandt <alunduil@gmail.com>
2018-12-26 22:01:24 -0800

    Update QuickCheck.

--- nix/network-uri-json.nix
+++ nix/network-uri-json.nix
@@ -1,18 +1,15 @@
-{ mkDerivation, aeson, base, fetchgit, hspec, network-uri
-, QuickCheck, stdenv, test-invariant, text
+{ mkDerivation, aeson, base, hspec, hspec-discover
+, network-arbitrary, network-uri, stdenv, test-invariant, text
 }:
 mkDerivation {
   pname = "network-uri-json";
-  version = "0.1.0.0";
-  src = fetchgit {
-    url = "https://github.com/alunduil/network-uri-json";
-    sha256 = "0cj8fz7733czd83qgr0wwqagdd3z6fy0c7g46mn88vmd2rwdscfz";
-    rev = "3fa5f2798a117f430a739b5d37958b48ff0450c9";
-  };
+  version = "0.2.0.0";
+  sha256 = "39dc79718af3c39acdaeb7cea22bc06b80a38f2b2eb5396b0c70d599adbdcac6";
   libraryHaskellDepends = [ aeson base network-uri text ];
   testHaskellDepends = [
-    aeson base hspec network-uri QuickCheck test-invariant text
+    aeson base hspec network-arbitrary network-uri test-invariant text
   ];
+  testToolDepends = [ hspec-discover ];
   homepage = "https://github.com/alunduil/network-uri-json";
   description = "FromJSON and ToJSON Instances for Network.URI";
   license = stdenv.lib.licenses.mit;
